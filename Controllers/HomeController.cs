da85e3a04536b6b8bd843396efa57207722b7c34
yogita <yogita3279@hotmail.com>
2020-09-16 13:33:06 -0700

    Multiple file upload code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,10 @@ using System.IO;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication4.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
 using System.Collections.Generic;
 
 
@@ -31,113 +35,136 @@ namespace WebApplication4.Controllers
             return View();
         }
         /// <summary>
-        /// Post method for importing users 
+        /// Post method for importing Data 
         /// </summary>
         /// <param name="postedFile"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase postedFile)
+        public ActionResult Index(HttpPostedFileBase[] postedFile)
         {
 
-            if (postedFile != null)
+            DataSet ds = GetDataTablesFromExcel(postedFile);
+            for (int i = 0; i < ds.Tables.Count; i++)
             {
-                try
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Constring"].ConnectionString))
                 {
-                    string fileExtension = Path.GetExtension(postedFile.FileName);
-
-                    //Validate uploaded file and return error.
-                    if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
                     {
-                        ViewBag.Message = "Please select the excel file with .xls or .xlsx extension";
-                        return View();
+                        sqlBulkCopy.DestinationTableName = ds.Tables[0].TableName;
+                        con.Open();
+                        sqlBulkCopy.WriteToServer(ds.Tables[0]);
+                        con.Close();
                     }
-
-                    string folderPath = Server.MapPath("~/UploadedFiles/");
-                    //Check Directory exists else create one
-                    if (!Directory.Exists(folderPath))
-                    {
-                        Directory.CreateDirectory(folderPath);
-                    }
-
-                    //Save file to folder
-                    var filePath = folderPath + Path.GetFileName(postedFile.FileName);
-                    postedFile.SaveAs(filePath);
-
-                    //Get file extension
-
-                    string excelConString = "";
-
-                    //Get connection string using extension 
-                    switch (fileExtension)
-                    {
-                        //If uploaded file is Excel 1997-2007.
-                        case ".xls":
-                            excelConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
-                            break;
-                        //If uploaded file is Excel 2007 and above
-                        case ".xlsx":
-                            excelConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
-                            break;
-                    }
-
-                    //Read data from first sheet of excel into datatable
-                    DataTable dt = new DataTable();
-                    excelConString = string.Format(excelConString, filePath);
-
-                    using (OleDbConnection excelOledbConnection = new OleDbConnection(excelConString))
-                    {
-                        using (OleDbCommand excelDbCommand = new OleDbCommand())
-                        {
-                            using (OleDbDataAdapter excelDataAdapter = new OleDbDataAdapter())
-                            {
-                                excelDbCommand.Connection = excelOledbConnection;
-
-                                excelOledbConnection.Open();
-                                //Get schema from excel sheet
-                                DataTable excelSchema = GetSchemaFromExcel(excelOledbConnection);
-                                //Get sheet name
-                                string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
-                                excelOledbConnection.Close();
-
-                                //Read Data from First Sheet.
-                                excelOledbConnection.Open();
-                                excelDbCommand.CommandText = "SELECT * From [" + sheetName + "]";
-                                excelDataAdapter.SelectCommand = excelDbCommand;
-                                //Fill datatable from adapter
-                                excelDataAdapter.Fill(dt);
-                                excelOledbConnection.Close();
-                            }
-                        }
-                    }
-
-                    GetSubmittedBusRouteDataFromExcelRow(dt);
-                    //Insert records to Submitted Route table.
-                    //using (var context = new DemoContext())
-                    //{
-                    //    var a = GetSubmittedBusRouteDataFromExcelRow(dt);
-                    //    context.STARS_SubmittedRouteData.Add(a);
-                    //    context.SaveChanges();
-                    //}
-                    ViewBag.Message = "Data Imported Successfully.";
-                }
-                catch (Exception ex)
-                {
-                    ViewBag.Message = ex.Message;
                 }
             }
-            else
-            {
-                ViewBag.Message = "Please select the file first to upload.";
-            }
+
             return View();
         }
 
+        private DataSet GetDataTablesFromExcel(HttpPostedFileBase[] postedFiles)
+        {
+            DataSet ds = new DataSet();
+            string path = Server.MapPath("~/UploadedFiles/");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            for (int j = 0; j < postedFiles.Length; j++)
+            {
+
+                if (postedFiles[j] != null)
+                {
+                    try
+                    {
+                        HttpPostedFileBase postedFile = postedFiles[j];
+                        string filePath = string.Empty;
+                        filePath = path + Path.GetFileName(postedFile.FileName);
+                        string fileExtension = Path.GetExtension(postedFile.FileName);
+
+                        //Validate uploaded file and return error.
+                        if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                        {
+                            ViewBag.Message = "Please select the excel file with .xls or .xlsx extension";
+                           // return Index();
+                        }
+
+                      
+
+                        //Save file to folder
+                        postedFile.SaveAs(filePath);
+
+                        //Get file extension
+
+                        string excelConString = "";
+
+                        //Get connection string using extension 
+                        switch (fileExtension)
+                        {
+                            //If uploaded file is Excel 1997-2007.
+                            case ".xls":
+                                excelConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+                                break;
+                            //If uploaded file is Excel 2007 and above
+                            case ".xlsx":
+                                excelConString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 8.0;HDR=YES'";
+                                break;
+                        }
+
+                        //Read data from first sheet of excel into datatable
+                        DataTable dt = new DataTable();
+                        excelConString = string.Format(excelConString, filePath);
+
+                        using (OleDbConnection excelOledbConnection = new OleDbConnection(excelConString))
+                        {
+                            using (OleDbCommand excelDbCommand = new OleDbCommand())
+                            {
+                                using (OleDbDataAdapter excelDataAdapter = new OleDbDataAdapter())
+                                {
+                                    excelDbCommand.Connection = excelOledbConnection;
+
+                                    excelOledbConnection.Open();
+                                    //Get schema from excel sheet
+                                    DataTable excelSchema = GetSchemaFromExcel(excelOledbConnection);
+                                    //Get sheet name
+                                    string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                    excelOledbConnection.Close();
+
+                                    //Read Data from First Sheet.
+                                    excelOledbConnection.Open();
+                                    excelDbCommand.CommandText = "SELECT * From [" + sheetName + "]";
+                                    excelDataAdapter.SelectCommand = excelDbCommand;
+                                    //Fill datatable from adapter
+                                    excelDataAdapter.Fill(dt);
+                                    excelOledbConnection.Close();
+                                }
+                            }
+                        }
+
+                        GetSubmittedBusRouteDataFromExcelRow(dt);
+                     
+                        ViewBag.Message = "Data Imported Successfully.";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Message = ex.Message;
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Please select the file first to upload.";
+                }
+                //return View();
+            }
+            Directory.Delete(path, true);
+
+            return ds;
+        }
         private static DataTable GetSchemaFromExcel(OleDbConnection excelOledbConnection)
         {
             return excelOledbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
         }
 
-        //private STARS_SubmittedRouteData GetSubmittedBusRouteDataFromExcelRow(DataTable dt)
         private void GetSubmittedBusRouteDataFromExcelRow(DataTable dt)
         {
             try
